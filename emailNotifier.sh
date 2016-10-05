01698d6342011f8c4a4e6da612d1c46990df53ce
Denis Quaid <denis.quaid@net-a-porter.com>
2016-10-05 15:20:07 +0100

    Js file that checks if newcastle tickets are up on the Brentford website and notifies me

---
+++ emailNotifier.sh
@@ -0,0 +1,1 @@
+mail -s "Newcastle Tickets Are Up" denisq91@gmail.com <<< "Newcastle Tickets Available At : https://tickets.brentfordfc.co.uk/PagesPublic/ProductBrowse/productHome.aspx"
---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var fs = require('fs');
+var request = require('request');
+var app = express();
+var spawn = require('child_process').spawn;
+
+var urlString = 'https://tickets.brentfordfc.co.uk/PagesPublic/ProductBrowse/productHome.aspx';
+var keyword = 'Barnsley';
+var cookieValue = '.ASPXANONYMOUS=6Ey_DpZV0gEkAAAAYzM4NzMxMDktMWY3ZS00MzU0LTkyNzYtY2FkYTA1ZDc4MjQ4pwgvMd0yVM5lqrpuZqpuvnQofOg1; ASP.NET_SessionId=zehciq3kekud3wubuao5ft1s';
+
+// Makes a request to orders page and calls the callback with html body to be processed
+request({
+	headers: {
+		'Cookie': cookieValue
+	},
+	uri: urlString,
+	body: '',
+	method: 'GET'
+}, function(err, res, body) {
+	if (!err) {
+		fs.writeFile('htmlResponse', body, function(err) {
+		});
+
+		if (body.includes(keyword)) {
+			console.log(keyword + " tickets are up!")
+			spawn('sh', ['emailNotifier.sh']);
+			console.log('email sent!')
+		} else {
+			console.log("No tickets yet!")
+		}
+	} else {
+		console.log('An error has occured');
+	}
+
+});
