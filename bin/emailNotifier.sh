4a7b55a289bcc93083d76c8f0d3d04b3431ccd4c
Denis Quaid <denis.quaid@net-a-porter.com>
2016-10-06 18:26:26 +0100

    Wrap the request in a function for scheduler

--- bin/emailNotifier.sh
+++
@@ -1,1 +0,0 @@
-mail -s "Newcastle Tickets Are Up" denisq91@gmail.com <<< "Newcastle Tickets Available At : https://tickets.brentfordfc.co.uk/PagesPublic/ProductBrowse/productHome.aspx"
--- server.js
+++ server.js
@@ -7,27 +7,30 @@ var keyword = 'Barnsley';
 var cookieValue = '.ASPXANONYMOUS=6Ey_DpZV0gEkAAAAYzM4NzMxMDktMWY3ZS00MzU0LTkyNzYtY2FkYTA1ZDc4MjQ4pwgvMd0yVM5lqrpuZqpuvnQofOg1; ASP.NET_SessionId=zehciq3kekud3wubuao5ft1s';
 
 // Makes a request to orders page and calls the callback with html body to be processed
-request({
-	headers: {
-		'Cookie': cookieValue
-	},
-	uri: urlString,
-	body: '',
-	method: 'GET'
-}, function(err, res, body) {
-	if (!err) {
-		fs.writeFile('htmlResponse', body, function(err) {
-		});
 
-		if (body.includes(keyword)) {
-			console.log(keyword + " tickets are up!")
-			spawn('sh', ['emailNotifier.sh']);
-			console.log('email sent!')
+function makeRequest() {
+	request({
+		headers: {
+			'Cookie': cookieValue
+		},
+		uri: urlString,
+		body: '',
+		method: 'GET'
+	}, function(err, res, body) {
+		if (!err) {
+			fs.writeFile('htmlResponse', body, function(err) {});
+
+			if (body.includes(keyword)) {
+				console.log(keyword + " tickets are up!")
+				spawn('sh', ['emailNotifier.sh']);
+				console.log('email sent!')
+			} else {
+				console.log("No tickets yet!")
+			}
 		} else {
-			console.log("No tickets yet!")
+			console.log('An error has occured');
 		}
-	} else {
-		console.log('An error has occured');
-	}
 
-});
+	});
+}
+makeRequest();
