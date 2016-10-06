f6e4433c2a38dbff842e336fc2faa81f871adab9
Denis Quaid <denis.quaid@net-a-porter.com>
2016-10-06 18:43:54 +0100

    Make a first request to activate the session cookie

--- server.js
+++ server.js
@@ -7,30 +7,46 @@ var keyword = 'Barnsley';
 var cookieValue = '.ASPXANONYMOUS=6Ey_DpZV0gEkAAAAYzM4NzMxMDktMWY3ZS00MzU0LTkyNzYtY2FkYTA1ZDc4MjQ4pwgvMd0yVM5lqrpuZqpuvnQofOg1; ASP.NET_SessionId=zehciq3kekud3wubuao5ft1s';
 
 // Makes a request to orders page and calls the callback with html body to be processed
-
 function makeRequest() {
+	// make a first request to activate the session cookie
 	request({
-		headers: {
-			'Cookie': cookieValue
-		},
-		uri: urlString,
-		body: '',
-		method: 'GET'
-	}, function(err, res, body) {
-		if (!err) {
-			fs.writeFile('htmlResponse', body, function(err) {});
+			headers: {
+				'Cookie': cookieValue
+			},
+			uri: urlString,
+			body: '',
+			method: 'GET'
+		}, function(err, res, body) {
+			if (!err) {
+				// make a second request to check for tickets
+				request({
+					headers: {
+						'Cookie': cookieValue
+					},
+					uri: urlString,
+					body: '',
+					method: 'GET'
+				}, function(err, res, body) {
+					if (!err) {
+						fs.writeFile('htmlResponse', body, function(err) {});
 
-			if (body.includes(keyword)) {
-				console.log(keyword + " tickets are up!")
-				spawn('sh', ['emailNotifier.sh']);
-				console.log('email sent!')
+						if (body.includes(keyword)) {
+							console.log(keyword + " tickets are up!")
+							spawn('sh', ['emailNotifier.sh']);
+							console.log('email sent!')
+						} else {
+							console.log("No tickets yet!")
+						}
+					} else {
+						console.log('An error has occured');
+					}
+
+				});
 			} else {
 				console.log("No tickets yet!")
 			}
-		} else {
-			console.log('An error has occured');
-		}
-
 	});
+
+
 }
 makeRequest();
