b903428c338abe41e74518571e6cad44ff88401f
Denis Quaid <denis.quaid@net-a-porter.com>
2016-10-06 11:11:08 +0100

    Add a script to be excecuted by Heroku scheduler

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
-var express = require('express');
 var fs = require('fs');
 var request = require('request');
-var app = express();
 var spawn = require('child_process').spawn;
 
 var urlString = 'https://tickets.brentfordfc.co.uk/PagesPublic/ProductBrowse/productHome.aspx';
