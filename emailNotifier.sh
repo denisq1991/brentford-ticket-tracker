cb62ac8605e9c39cfbb5ef51f8eb5e64aee1cee8
Denis Quaid <denis.quaid@net-a-porter.com>
2016-10-10 11:10:23 +0100

    Set the full path to the shell script so it works within cron

--- emailNotifier.sh
+++ emailNotifier.sh
@@ -1,1 +1,3 @@
+#!/bin/sh
+
 mail -s "Newcastle Tickets Are Up" denisq91@gmail.com <<< "Newcastle Tickets Available At : https://tickets.brentfordfc.co.uk/PagesPublic/ProductBrowse/productHome.aspx"
--- server.js
+++ server.js
@@ -3,50 +3,49 @@ var request = require('request');
 var spawn = require('child_process').spawn;
 
 var urlString = 'https://tickets.brentfordfc.co.uk/PagesPublic/ProductBrowse/productHome.aspx';
-var keyword = 'Barnsley';
+var keyword = 'Newcastle';
 var cookieValue = '.ASPXANONYMOUS=6Ey_DpZV0gEkAAAAYzM4NzMxMDktMWY3ZS00MzU0LTkyNzYtY2FkYTA1ZDc4MjQ4pwgvMd0yVM5lqrpuZqpuvnQofOg1; ASP.NET_SessionId=zehciq3kekud3wubuao5ft1s';
 
 // Makes a request to orders page and calls the callback with html body to be processed
 function makeRequest() {
 	// make a first request to activate the session cookie
 	request({
-			headers: {
-				'Cookie': cookieValue
-			},
-			uri: urlString,
-			body: '',
-			method: 'GET'
-		}, function(err, res, body) {
-			if (!err) {
-				// make a second request to check for tickets
-				request({
-					headers: {
-						'Cookie': cookieValue
-					},
-					uri: urlString,
-					body: '',
-					method: 'GET'
-				}, function(err, res, body) {
-					if (!err) {
-						fs.writeFile('htmlResponse', body, function(err) {});
-
-						if (body.includes(keyword)) {
-							console.log(keyword + " tickets are up!")
-							spawn('sh', ['emailNotifier.sh']);
-							console.log('email sent!')
-						} else {
-							console.log("No tickets yet!")
-						}
+		headers: {
+			'Cookie': cookieValue
+		},
+		uri: urlString,
+		body: '',
+		method: 'GET'
+	}, function(err, res, body) {
+		if (!err) {
+			// make a second request to check for tickets
+			request({
+				headers: {
+					'Cookie': cookieValue
+				},
+				uri: urlString,
+				body: '',
+				method: 'GET'
+			}, function(err, res, body) {
+				if (!err) {
+					// save the response to a file to manually inspect it
+					fs.writeFile('htmlResponse', body, function(err) {});
+					if (body.includes(keyword)) {
+						console.log(keyword + " tickets are up!")
+						// set this to the full path of your email script if you want to run this on a cron
+						spawn('sh', ['/Users/d.quaid/work/brentford-ticket-tracker/emailNotifier.sh']);
+						console.log('email sent!')
 					} else {
-						console.log('An error has occured');
+						console.log("No tickets yet!")
 					}
+				} else {
+					console.log('An error has occured');
+				}
 
-				});
-			} else {
-				console.log("No tickets yet!")
-			}
+			});
+		} else {
+			console.log("No tickets yet!")
+		}
 	});
-
-
 }
 makeRequest();
